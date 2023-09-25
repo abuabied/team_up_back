43ce28b5710136dfebca35c28fdddf784e8c98ce
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-09-25 13:27:00 +0300

    update request origin

--- src/main/java/com/abuabied/teamUp/Controllers/UserController.java
+++ src/main/java/com/abuabied/teamUp/Controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin(maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", exposedHeaders = "*", allowCredentials = "true", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
