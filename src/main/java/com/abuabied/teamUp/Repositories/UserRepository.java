8424c5a557e164816fe636e8c663135bbd0c12b4
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-09-25 16:23:45 +0300

    update

--- src/main/java/com/abuabied/teamUp/Repositories/UserRepository.java
+++ src/main/java/com/abuabied/teamUp/Repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.abuabied.teamUp.Repositories;
 
 import com.abuabied.teamUp.Entities.User;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
