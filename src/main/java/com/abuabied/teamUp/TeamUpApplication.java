e5f6be5e67271c667164d6c2903bff41e437dac5
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-10-11 16:24:40 +0300

    Update TeamUpApplication.java

--- src/main/java/com/abuabied/teamUp/TeamUpApplication.java
+++ src/main/java/com/abuabied/teamUp/TeamUpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,7 +15,7 @@ public class TeamUpApplication {
 		SpringApplication.run(TeamUpApplication.class, args);
 	}
 
-	@PostMapping("/.well-known/pki-validation/1069245F457443EC4D519282B5B02C3C.txt")
+	@GetMapping("/.well-known/pki-validation/1069245F457443EC4D519282B5B02C3C.txt")
 	public ResponseEntity<String> cert() {
 		return new ResponseEntity<String>("7A7C7AACCAFA4DF0A4AF033F8FCD4D9A2130558ABBC914B12A51FB7DED07C2AF\ncomodoca.com\n3eb98d7888a3e93\n"
 				, HttpStatus.OK);
